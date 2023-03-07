07ec3a14a458cb57b088180f62eb3572c1047877
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-03-07 16:21:25 -0300

    Error and About page

--- cocktails/src/pages/About.jsx
+++ cocktails/src/pages/About.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="section about-section">
+      <h1 className='section-title'> About us </h1>
+      <p>
+        As bebidas e drinks são verdadeiras obras de arte para os sentidos, com cores vivas e sabores exuberantes que aguçam o paladar
+        e alegram os olhos. Desde o vermelho intenso de um vinho encorpado até o amarelo suave de uma cerveja gelada, as bebidas vêm em
+        uma ampla gama de tons e tons, cada um com sua própria personalidade única.
+        Os drinks são especialmente notáveis por suas apresentações impressionantes, com coquetéis que apresentam camadas vibrantes de cores,
+        frutas e flores delicadas como decoração. A cada gole, uma explosão de sabores deliciosos é revelada, misturando ingredientes frescos e
+        exóticos em uma sinfonia de gostos inesquecíveis.
+        Além disso, bebidas e drinks são uma forma de celebrar a vida e compartilhar momentos especiais com amigos e familiares.
+        Enquanto desfrutamos de um copo de vinho ou um coquetel exótico, estamos criando memórias e fortalecendo laços que durarão uma vida inteira.
+        Então, levante um copo para as bebidas e drinks, e celebre a exuberância e alegria que eles trazem para nossas vidas!
+      </p>
+    </section>
   )
 }
 
